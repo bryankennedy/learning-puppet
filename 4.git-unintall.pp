83f79497a624f31f859b75c065f9b6e6d07db723
bryan kennedy <bkennedy@smm.org>
2013-03-21 18:48:15 -0500

    Seeing if absent work better than uninstalled

--- 4.git-unintall.pp
+++ 4.git-unintall.pp
@@ -1,5 +1,5 @@
 package { 'Git version Git-1.8.1.2-preview20130201':
-  ensure            => uninstalled,
+  ensure            => absent,
   uninstall_options => ['/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES'],
 }
 
