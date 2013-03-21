44dd58bc812c0952e9d0e419d15baf8d874cbd70
bryan kennedy <bkennedy@smm.org>
2013-03-21 18:35:26 -0500

    Fixing name of package to install
    Testing whether this will fix the re-install problem

--- 3.package.pp
+++ 3.package.pp
@@ -1,6 +1,6 @@
 # Ensure that Git is installed
 # This does nothing to make sure that this file is on the system
-package { 'git':
+package { 'Git version Git-1.8.1.2-preview20130201':
   ensure          => installed,
   source          => 'c:/depot/Git-1.8.1.2-preview20130201.exe',
   install_options => [
