a8becbe60d480f675791271f1d7dc9c6e07b0ea0
bryan kennedy <bkennedy@smm.org>
2013-03-21 16:22:51 -0500

    Striping git install to its basics
    I think this should work now that this file is in place, but this will
    test that.

--- 3.package.pp
+++ 3.package.pp
@@ -1,16 +1,6 @@
-#exec { "":
-#  command => "wget https://msysgit.googlecode.com/files/Git-1.8.1.2-preview20130201.exe"
-#  cwd     => "/dev/tmp",
-#  creates => "/var/tmp/git-installer.exe",
-#  path    => ["/usr/bin", "/usr/sbin"]
-#}
-
-exec { 'cmd.exe /c echo hello world':
-  path => $::path
+# Ensure that Git is installed
+# This does nothing to make sure that this file is on the system
+package { 'git':
+  ensure          => installed,
+  source          => 'c:/depot/Git-1.8.1.2-preview20130201.exe',
 }
-
-#package { 'git':
-#  ensure          => installed,
-#  source          => 'N/:packages/mysql-5.5.16-winx64.msi',
-#  install_options => { 'INSTALLDIR' => 'C:\mysql-5.5' },
-#}
